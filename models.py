633565f82d48e4663281db1a9a26e0185f73473c
Sarthak-04 <4sarthakagrawal@gmail.com>
2016-12-24 23:37:19 +0530

    changes to model

--- models.py
+++ models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class Record(models.Model):
+	first_image_name = models.CharField(max_length=50)
+	second_image_name = models.CharField(max_length=50)
+	first_image_votes = models.PositiveIntegerField()
+	second_image_votes = models.PositiveIntegerField()
+	first_image = models.ImageField(upload_to = 'pic_folder/', default = 'pic_folder/None/image_one.jpg')
+	second_image = models.ImageField(upload_to = 'pic_folder/', default = 'pic_folder/None/image_two.jpg')
+	
